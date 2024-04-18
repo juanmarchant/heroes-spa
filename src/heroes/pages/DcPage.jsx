055d0d07ebd8489e220601f918eb6066b76425b8
juanmarchant <jua.marchant@duocuc.cl>
2024-04-18 18:35:42 -0400

    second router added - helpers -routes-data- list of some heroes <li>

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,7 +1,13 @@
-
+import { HeroList } from "../components"
 
 export const DcPage = () => {
   return (
-    <h1>DcPage</h1>
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+      
+      {/* Listado de heroes */}
+      <HeroList publisher={'DC Comics'}/>
+    </>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,5 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -14,17 +24,31 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink
-                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/dc"
                     >
                         DC
+                    </NavLink>
+
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
+
+                    <NavLink
+                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/hero"
+                    >
+                        Hero
                     </NavLink>
                 </div>
             </div>
@@ -34,7 +58,7 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-primary'>
                         Juan
                     </span>
-                    <button className='nav-item nav-link btn'>Logout</button>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>Logout</button>
                 </ul>
             </div>
         </nav>
