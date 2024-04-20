25bcc8276d2846c23c14212533c59ca53c0445d4
juanmarchant <jua.marchant@duocuc.cl>
2024-04-20 00:17:52 -0400

    done

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -58,7 +58,7 @@ export const SearchPage = () => {
             (q === '')
               ? <div className="alert alert-primary animate__animated animate__fadeIn">Search a hero</div>
               : (heroes.length === 0)
-              && <div className="alert alert-dange animate__animated animate__fadeIn">No hero with <b>{q}</b></div>
+              && <div className="alert alert-danger animate__animated animate__fadeIn">No hero with <b>{q}</b></div>
           }
 
 
